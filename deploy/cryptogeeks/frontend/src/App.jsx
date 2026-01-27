1732dcc2b147f9b76bacffa13cc403ab903166c7
GrCOTE7 <grcote7@gmail.com>
2026-01-27 18:09:43 +0100

    Larger CryptosGeeks title

--- deploy/cryptogeeks/frontend/src/App.jsx
+++ deploy/cryptogeeks/frontend/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <main className="page">
       <section className="card">
-        <p className="text-2xl tag">CryptosGeeks.com</p>
+        <p className="text-3xl tag">CryptosGeeks.com</p>
         <h1>Site en construction...</h1>
         <p className="subtitle italic">
           Restez en alerte... Et revenez très bientôt !
