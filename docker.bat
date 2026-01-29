56ed64cee5a34a1db399efe31d8fe8201d0c8f9c
GrCOTE7 <grcote7@gmail.com>
2026-01-29 19:33:33 +0100

    Add starter docker that starts cryptosgeeks too

--- docker.bat
+++ docker.bat
@@ -2,10 +2,21 @@
 
 call stop.bat
 
+
 REM Démarre les services Docker (dev) avec build
 pushd "%~dp0"
 @REM .exe: Appel explicite pour pas de conflit avec docker.bat
 docker.exe compose -f docker-compose.dev.yml up --build -d
 set EXITCODE=%ERRORLEVEL%
 popd
-exit /b %EXITCODE%
+
+REM Démarre aussi Cryptogeeks (CGC) en local
+pushd "%~dp0deploy\cryptogeeks"
+docker.exe compose -f docker-compose.dev.yml up --build -d
+set EXITCODE2=%ERRORLEVEL%
+popd
+
+REM Combine les codes de sortie (si l'un échoue, EXITCODE sera non nul)
+if %EXITCODE% NEQ 0 exit /b %EXITCODE%
+if %EXITCODE2% NEQ 0 exit /b %EXITCODE2%
+exit /b 0
