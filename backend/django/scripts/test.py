598267d23473f294c2188cd2177c3bfce513cfaa
GrCOTE7 <grcote7@gmail.com>
2026-02-07 17:12:26 +0100

    3 way to run docker script

---
+++ backend/django/scripts/test.py
@@ -0,0 +1,31 @@
+import django, os, sys
+
+# Exécution : docker exec -it celery_worker python scripts/test.py
+
+# Ajouter la racine Django au PYTHONPATH
+BASE_DIR =e os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
+# Indiquer les settings Django
+os.environ.setdfault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+django.setup()
+print(django.conf.settings.INSTALLED_APPS)
+
+# ----
+
+# Ou, directement inline :
+
+# docker exec -it celery_worker python -c "import django; django.setup(); print(django.conf.settings.INSTALLED_APPS)"
+
+# ----
+
+# Ou, en shell dans le contnr :
+
+# docker exec -it celery_worker sh
+
+# python
+
+# import django
+# django.setup()
+# print(django.conf.settings.INSTALLED_APPS)
