4fb787843841d2ab0cdb49cb9487d2a2b1ed4901
GrCOTE7 <grcote7@gmail.com>
2026-02-07 17:16:02 +0100

    Corr

--- backend/django/scripts/test.py
+++ backend/django/scripts/test.py
@@ -3,29 +3,22 @@ import django, os, sys
 # Exécution : docker exec -it celery_worker python scripts/test.py
 
 # Ajouter la racine Django au PYTHONPATH
-BASE_DIR =e os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
 # Indiquer les settings Django
-os.environ.setdfault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 django.setup()
 print(django.conf.settings.INSTALLED_APPS)
 
 # ----
-
 # Ou, directement inline :
-
 # docker exec -it celery_worker python -c "import django; django.setup(); print(django.conf.settings.INSTALLED_APPS)"
-
 # ----
-
 # Ou, en shell dans le contnr :
-
 # docker exec -it celery_worker sh
-
 # python
-
 # import django
 # django.setup()
 # print(django.conf.settings.INSTALLED_APPS)
