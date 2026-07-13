88c9fb08d0dfbd6e737d666b50a51c62bedb7719
Aegis Test <test@aegis-stack.dev>
2026-07-13 21:48:58 +0200

    test with &&

--- frontend/src/pages/Learn.jsx
+++ frontend/src/pages/Learn.jsx
@@ -8,7 +8,7 @@ function About() {
     setDocumentTitle("Learn");
   }, []);
 
-  const localUse = isLocalhost ? "local (localhost)" : "Production (non localhost)";
+  const localUse = isLocalhost ? "local" : "Production";
   
   const buttonStyle =
     "button bg-yellow-100 text-blue-600 px-2 py-1 rounded hover:bg-blue-100 hover:text-blue-800 hover:font-bold transition-colors duration-700";
@@ -35,7 +35,9 @@ function About() {
       <main className="flex items-center justify-center min-h-screen bg-slate-50 text-slate-800 font-[Roboto]">
         <section className="border max-w-2xl w-full mx-6 p-8 text-center bg-white rounded-2xl shadow-2xl">
           <h1 className="text-3xl font-bold text-blue-600 mb-4">Learn</h1>
-          <p className="text-lg text-slate-600 font-bold">Page de test - Mode {localUse}</p>
+          <p className="text-lg text-slate-600 font-bold">
+            Page de test - Mode {localUse}<br />
+          {isLocalhost && "Localhost"}</p>
 
           <div className="mt-6">
             <AButtonWithoutAction />
