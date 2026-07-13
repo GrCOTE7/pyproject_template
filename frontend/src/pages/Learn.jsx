0a8490ee7615ad35d785f72e18bc52ac0d200bae
Aegis Test <test@aegis-stack.dev>
2026-07-13 21:08:03 +0200

    cinditional screen

--- frontend/src/pages/Learn.jsx
+++ frontend/src/pages/Learn.jsx
@@ -1,12 +1,15 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { setDocumentTitle } from "../utils/documentTitle";
+import { isLocalhost } from "../auth";
 
 function About() {
   useEffect(() => {
     setDocumentTitle("Learn");
   }, []);
 
+  const localUse = isLocalhost ? "local (localhost)" : "Production (non localhost)";
+  
   const buttonStyle =
     "button bg-yellow-100 text-blue-600 px-2 py-1 rounded hover:bg-blue-100 hover:text-blue-800 hover:font-bold transition-colors duration-700";
 
@@ -32,7 +35,7 @@ function About() {
       <main className="flex items-center justify-center min-h-screen bg-slate-50 text-slate-800 font-[Roboto]">
         <section className="border max-w-2xl w-full mx-6 p-8 text-center bg-white rounded-2xl shadow-2xl">
           <h1 className="text-3xl font-bold text-blue-600 mb-4">Learn</h1>
-          <p className="text-lg text-slate-600 font-bold">Page de test</p>
+          <p className="text-lg text-slate-600 font-bold">Page de test - Mode {localUse}</p>
 
           <div className="mt-6">
             <AButtonWithoutAction />
